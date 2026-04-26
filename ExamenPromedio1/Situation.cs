54c96c25c816d8df72fd2db57ebb72f426aec66d
Yabely Gutierrez <yanlie.arts@gmail.com>
2026-04-26 03:18:36 -0500

    Creación de clase Situation

---
+++ ExamenPromedio1/Situation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenPromedio1
+{
+    internal class Situation
+    {
+
+        public string description;
+        public List<Option> options;
+
+        public Situation(string description)
+        {
+            this.description = description;
+            options = new List<Option>();
+        }
+
+        public virtual void ExecuteOption(Player player)
+        {
+            Console.WriteLine(description);
+        }
+
+
+    }
+}
