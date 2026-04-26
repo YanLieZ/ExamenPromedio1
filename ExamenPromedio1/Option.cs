90e6012597cc638c90b3ab1f04132059d9a3aeb3
Yabely Gutierrez <yanlie.arts@gmail.com>
2026-04-26 03:00:58 -0500

    Creación de clase option y creación de variables

---
+++ ExamenPromedio1/Option.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenPromedio1
+{
+    internal class Option
+    {
+
+        public string text;
+        public int healthChange;
+        public Situation nextSituation;
+
+        public Option(string text, int healthChange, Situation next)
+        {
+            this.text = text;
+            this.healthChange = healthChange;
+            nextSituation = next;
+        }
+
+
+
+
+
+
+    }
+}
