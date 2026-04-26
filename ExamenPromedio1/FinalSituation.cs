1fed872ee3fbf448e78776c3935397406b67b2f5
Yabely Gutierrez <yanlie.arts@gmail.com>
2026-04-26 03:26:59 -0500

    Creación  y construcción de las clases CombatSituation y FinalSituation
    Asignación de variables y herencia con Situation

---
+++ ExamenPromedio1/FinalSituation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExamenPromedio1
+{
+    internal class FinalSituation : Situation
+    {
+        public string endingType;
+
+        public FinalSituation(string description, string endingType) : base(description)
+        {
+            this.endingType = endingType;
+        }
+
+
+
+    }
+}
